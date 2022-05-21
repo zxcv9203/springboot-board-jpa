1fa844a3d15518e7a394bd47a3bcce2de428360b
yongckim <zxcv9203@naver.com>
2022-05-21 21:48:16 +0900

    jpa

--- src/test/java/com/devcourse/springjpaboard/order/controller/OrderControllerTest.java
+++ src/test/java/com/devcourse/springjpaboard/order/controller/OrderControllerTest.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigurationRestDocs
 @AutoConfigureMockMvc
 @SpringBootTest
 class OrderControllerTest {
